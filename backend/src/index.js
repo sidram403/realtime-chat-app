1f17d1374047836cebd1684f90271094ae4538cb
sidram403 <siddharthpatil9108@gmail.com>
2024-12-01 20:28:14 +0530

    structured backend files and added authentication api also backend code

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from 'dotenv'
+import cookieParser from "cookie-parser"
+
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
+
+dotenv.config()
+
+const app = express();
+
+const PORT = process.env.PORT
+
+app.use(express.json()) // to read inputs 
+app.use(cookieParser()) // grab the token from cookies
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+    connectDB()
+})
