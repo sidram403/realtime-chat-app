4d1285bc0cb4e6dad3c7c0024b2bd3a63276771c
sidram403 <siddharthpatil9108@gmail.com>
2024-12-01 21:01:47 +0530

    added message api and backend code

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js";
 
 dotenv.config()
@@ -15,6 +16,7 @@ app.use(express.json()) // to read inputs
 app.use(cookieParser()) // grab the token from cookies
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
